6fa7a88ba2947207f6938ae65db7091a2aafc413
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-03-18 12:31:34 +0330

    final3

---
+++ 03_reviews/setup/app.js
@@ -0,0 +1,33 @@
+const reviews = [
+ {
+  id: 1,
+  author: 'peter',
+  job: 'front-end',
+  image: 'images/peter.jpg',
+  info: "dolor, sit amet consectetur adipisicing elit. Tempore, deleniti! Est quo dolorum, maxime minima, temporibus quibusdam in nostrum optio obcaecati iure consectetur eum "
+ },
+
+ {
+  id: 2,
+  author: 'susan',
+  job: 'back-end',
+  image: 'images/susan.jpg',
+  info: "dolor, sit  minima, deleniti! Est quo dolorum, maxime minima, temporibus quibusdam in nostrum optio obcaecati iure consectetur eum temporibus quibusdam in nostrum optio obcaecati iure consectetur eum "
+ },
+
+ {
+  id: 3,
+  author: 'anna',
+  job: 'UI-UX',
+  image: 'images/anna.jpg',
+  info: "dolor, sit  in nostrum optio obcaecati iure consectetur eum temporibus quibusdam in nostrum optio obcaecati iure consectetur eum "
+ },
+
+ {
+  id: 4,
+  author: 'bill',
+  job: 'database',
+  image: 'images/bill.jpg',
+  info: "dolor, sit in nostrum optio obcaecati iure consectetur eum "
+ },
+]
