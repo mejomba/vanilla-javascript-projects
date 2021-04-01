03ff9cba9bf4e61e6d03fa7f02efcc8ed7dfddea
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-04-01 10:15:09 +0430

    11

---
+++ 11_tabs/final/app.js
@@ -0,0 +1,19 @@
+const about = document.querySelector('.about');
+const btns = document.querySelectorAll('.tab-btn');
+const content = document.querySelectorAll('.content');
+
+about.addEventListener('click', function(e) {
+ const id = e.target.dataset.id;
+ const element = document.getElementById(id);
+
+ btns.forEach(function(btn) {
+  btn.classList.remove('active');
+ });
+ e.target.classList.add('active');
+ 
+ content.forEach(function(item) {
+  item.classList.remove('active');
+ });
+ element.classList.add('active');
+ 
+});
