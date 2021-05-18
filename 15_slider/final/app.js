3c3cb1b54db4286b18ee2d7709f2db45601a6005
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-05-18 18:54:32 +0430

    15 final

--- 15_slider/final/app.js
+++ 15_slider/final/app.js
@@ -14,25 +14,25 @@ slides.forEach(function(slide, idx) {
     slide.style.left = `${idx * 100}%`;
 })
 function carousel() {
-    // if (counter < 0) {
-    //     counter = slides.length - 1;
-    // } else if (counter === slides.length) {
-    //     counter = 0
+    if (counter < 0) {
+        counter = slides.length - 1;
+    } else if (counter === slides.length) {
+        counter = 0
+    }
+    // if (counter > 0) {
+    //     prevBtn.style.display = 'block';
+    // } else {
+    //     prevBtn.style.display = 'none';
     // }
-    if (counter > 0) {
-        prevBtn.style.display = 'block';
-    } else {
-        prevBtn.style.display = 'none';
-    }
 
-    if (counter === slides.length - 1) {
-        nextBtn.style.display = 'none';
-    } else {
-        nextBtn.style.display = 'block';
-    }
+    // if (counter === slides.length - 1) {
+    //     nextBtn.style.display = 'none';
+    // } else {
+    //     nextBtn.style.display = 'block';
+    // }
     slides.forEach(function(slide) {
         slide.style.transform = `translateX(-${counter * 100}%)`;
         console.log(counter);
     })
 }
-prevBtn.style.display = "none";
+// prevBtn.style.display = "none";
